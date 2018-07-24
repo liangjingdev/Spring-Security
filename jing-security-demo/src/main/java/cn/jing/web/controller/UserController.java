3d5ed0e10634e3575b0e04e046cc9752c53e80f4
梁竞 <1184106223@qq.com>
2018-07-24 18:13:55 +0800

    用户控制类、

---
+++ jing-security-demo/src/main/java/cn/jing/web/controller/UserController.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package cn.jing.web.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import cn.jing.dto.User;
+import cn.jing.dto.UserQueryCondition;
+
+/**
+ * @author liangjing
+ *
+ */
+@RestController
+public class UserController {
+
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public List<User> query(UserQueryCondition condition,
+			@PageableDefault(page = 2, size = 17, sort = "username,asc") Pageable pageable) {
+
+		System.out.println(ReflectionToStringBuilder.toString(condition, ToStringStyle.MULTI_LINE_STYLE));
+
+		System.out.println(pageable.getPageSize());
+		System.out.println(pageable.getPageNumber());
+		System.out.println(pageable.getSort());
+
+		List<User> users = new ArrayList<>();
+		users.add(new User());
+		users.add(new User());
+		users.add(new User());
+		return users;
+	}
+
+}
