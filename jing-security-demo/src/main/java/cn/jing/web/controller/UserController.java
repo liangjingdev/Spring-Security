2b496c62e1333104be64b67b04b2c680afcf8a74
梁竞 <1184106223@qq.com>
2018-07-26 22:39:18 +0800

    用户控制器类、

--- jing-security-demo/src/main/java/cn/jing/web/controller/UserController.java
+++ jing-security-demo/src/main/java/cn/jing/web/controller/UserController.java
@@ -6,14 +6,21 @@ package cn.jing.web.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonView;
 
 import cn.jing.dto.User;
 import cn.jing.dto.UserQueryCondition;
@@ -23,9 +30,11 @@ import cn.jing.dto.UserQueryCondition;
  *
  */
 @RestController
+@RequestMapping(value = "/user")
 public class UserController {
 
-	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	@GetMapping
+	@JsonView(User.UserSimpleView.class)
 	public List<User> query(UserQueryCondition condition,
 			@PageableDefault(page = 2, size = 17, sort = "username,asc") Pageable pageable) {
 
@@ -40,6 +49,37 @@ public class UserController {
 		users.add(new User());
 		users.add(new User());
 		return users;
+	}
+
+	/**
+	 * function:@PathVariable表示映射URL片段到Java方法的参数（在url声明中可以使用正则表达式）
+	 * 下面的方法URL片段进行了正则表达式的验证，ID只能是数字。
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@GetMapping("/{id:\\d+}")
+	@JsonView(User.UserDetailView.class)
+	public User getInfo(@PathVariable String id) {
+		User user = new User();
+		user.setUsername("tom");
+		return user;
+	}
+
+	@PostMapping
+	public User create(@Valid @RequestBody User user, BindingResult errors) {
+
+		// 如果发生检验错误，那么就将所有发生错误的信息逐个打印出来
+		if (errors.hasErrors()) {
+			errors.getAllErrors().forEach(error -> System.out.println(error.getDefaultMessage()));
+		}
+
+		System.out.println(user.getId());
+		System.out.println(user.getUsername());
+		System.out.println(user.getPassword());
+		System.out.println(user.getBirthday());
+		user.setId("1");
+		return user;
 	}
 
 }
