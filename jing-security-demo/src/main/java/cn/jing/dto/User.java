f83cca4d1eace039d77d2c5758ba4b5561b5745c
梁竞 <1184106223@qq.com>
2018-07-26 22:38:35 +0800

    用户实体类、

--- jing-security-demo/src/main/java/cn/jing/dto/User.java
+++ jing-security-demo/src/main/java/cn/jing/dto/User.java
@@ -3,16 +3,54 @@
  */
 package cn.jing.dto;
 
+import java.io.Serializable;
+import java.util.Date;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
 /**
  * @author liangjing
  *
  */
 public class User {
 
+	public interface UserSimpleView {
+	};
+
+	public interface UserDetailView extends UserSimpleView {
+	};
+
+	@NotEmpty(message = "用户名不能为空")
 	private String username;
 
+	@NotEmpty(message = "密码不能为空")
 	private String password;
 
+	private String id;
+
+	private Date birthday;
+
+	@JsonView(UserSimpleView.class)
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	@JsonView(UserSimpleView.class)
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@JsonView(UserSimpleView.class)
 	public String getUsername() {
 		return username;
 	}
@@ -21,6 +59,7 @@ public class User {
 		this.username = username;
 	}
 
+	@JsonView(UserDetailView.class)
 	public String getPassword() {
 		return password;
 	}
