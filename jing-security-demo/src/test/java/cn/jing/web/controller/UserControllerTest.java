8678383269d0bd70427ddab937f573e687426822
梁竞 <1184106223@qq.com>
2018-07-26 22:39:42 +0800

    用户控制器测试类、

--- jing-security-demo/src/test/java/cn/jing/web/controller/UserControllerTest.java
+++ jing-security-demo/src/test/java/cn/jing/web/controller/UserControllerTest.java
@@ -3,6 +3,8 @@
  */
 package cn.jing.web.controller;
 
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -38,11 +40,47 @@ public class UserControllerTest {
 
 	@Test
 	public void whenQuerySuccess() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/user").param("username", "liangjing").param("age", "18")
-				.param("ageTo", "60").param("xxx", "yyy")
-				// 查询第3页的数据，每页返回15条数据，然后根据年龄降序排序
-				//.param("size", "15").param("page", "3").param("sort", "age,desc")
-				.contentType(MediaType.APPLICATION_JSON_UTF8)).andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(3));
+		String result = mockMvc
+				.perform(MockMvcRequestBuilders.get("/user").param("username", "liangjing").param("age", "18")
+						.param("ageTo", "60").param("xxx", "yyy")
+						// 查询第3页的数据，每页返回15条数据，然后根据年龄降序排序
+						// .param("size", "15").param("page", "3").param("sort", "age,desc")
+						.contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(3)).andReturn().getResponse()
+				.getContentAsString();
+
+		System.out.println(result);
+	}
+
+	@Test
+	public void whenGetInfoSuccess() throws Exception {
+		String result = mockMvc
+				.perform(MockMvcRequestBuilders.get("/user/1").contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.username").value("tom")).andReturn().getResponse()
+				.getContentAsString();
+		System.out.println(result);
+	}
+
+	@Test
+	public void whenGetInfoFail() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/user/a").contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(MockMvcResultMatchers.status().is4xxClientError());
+	}
+
+	@Test
+	public void whenCreateSuccess() throws Exception {
+		Date date = new Date();
+		System.out.println(date.getTime());
+		String content = "{\"username\":\"tom\",\"password\":null,\"birthday\":" + date.getTime() + "}";
+		String result = mockMvc
+				.perform(MockMvcRequestBuilders.post("/user").contentType(MediaType.APPLICATION_JSON_UTF8)
+						.content(content))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id").value("1")).andReturn().getResponse()
+				.getContentAsString();
+
+		System.out.println(result);
 	}
 }
