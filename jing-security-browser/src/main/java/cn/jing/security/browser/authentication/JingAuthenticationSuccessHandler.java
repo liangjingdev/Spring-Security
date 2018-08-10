2fc29fcbcf018d9f8a4768872b4eacdaf82048d2
梁竞 <1184106223@qq.com>
2018-08-10 23:28:27 +0800

    登录成功处理器、

---
+++ jing-security-browser/src/main/java/cn/jing/security/browser/authentication/JingAuthenticationSuccessHandler.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package cn.jing.security.browser.authentication;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import cn.jing.security.core.properties.LoginType;
+import cn.jing.security.core.properties.SecurityProperties;
+
+/**
+ * @author liangjing
+ *
+ */
+@Component("jingAuthenticationSuccessHandler")
+public class JingAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private SecurityProperties securityProperties;
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+
+		logger.info("登录成功");
+
+		if (LoginType.JSON.equals(securityProperties.getBrowser().getLoginType())) {
+			response.setContentType("application/json;charset=UTF-8");
+			response.getWriter().write(objectMapper.writeValueAsString(authentication));
+		} else {
+			super.onAuthenticationSuccess(request, response, authentication);
+		}
+
+	}
+
+}
