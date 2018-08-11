1acc4854df932adf957c0107b733db05aaa53704
梁竞 <1184106223@qq.com>
2018-08-11 21:20:48 +0800

    验证码功能属性配置、

---
+++ jing-security-core/src/main/java/cn/jing/security/core/properties/ValidateCodeProperties.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package cn.jing.security.core.properties;
+
+/**
+ * @author liangjing
+ *
+ */
+public class ValidateCodeProperties {
+
+	private ImageCodeProperties image = new ImageCodeProperties();
+
+	public ImageCodeProperties getImage() {
+		return image;
+	}
+
+	public void setImage(ImageCodeProperties image) {
+		this.image = image;
+	}
+
+}
