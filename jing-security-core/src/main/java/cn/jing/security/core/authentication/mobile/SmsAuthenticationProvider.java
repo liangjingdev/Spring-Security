98a8fb6effcc56caa2c7d1d9a97164f187521ae7
梁竞 <1184106223@qq.com>
2018-09-07 22:44:12 +0800

    短信登录验证逻辑

---
+++ jing-security-core/src/main/java/cn/jing/security/core/authentication/mobile/SmsAuthenticationProvider.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package cn.jing.security.core.authentication.mobile;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * function:短信登录验证逻辑。 由于短信验证码的验证在SmsCodeFilter过滤器里已完成，所以这里直接读取用户信息即可。
+ * 
+ * @author liangjing
+ *
+ */
+public class SmsAuthenticationProvider implements AuthenticationProvider {
+
+	private UserDetailsService userDetailsService;
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+		SmsCodeAuthenticationToken authenticationToken = (SmsCodeAuthenticationToken) authentication;
+
+		// 获取用户信息
+		UserDetails user = userDetailsService.loadUserByUsername((String) authenticationToken.getPrincipal());
+
+		if (user == null) {
+			throw new InternalAuthenticationServiceException("无法获取用户信息");
+		}
+
+		SmsCodeAuthenticationToken authenticationResult = new SmsCodeAuthenticationToken(user, user.getAuthorities());
+
+		authenticationResult.setDetails(authenticationToken.getDetails());
+
+		return authenticationResult;
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		// 判断传过来的authentication是不是SmsCodeAuthenticationToken类型
+		return SmsCodeAuthenticationToken.class.isAssignableFrom(authentication);
+	}
+
+	public UserDetailsService getUserDetailsService() {
+		return userDetailsService;
+	}
+
+	public void setUserDetailsService(UserDetailsService userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
+
+}
