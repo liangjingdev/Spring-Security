fe774e446df5b82e13ede31a1810195502737512
梁竞 <1184106223@qq.com>
2018-09-21 10:35:39 +0800

    资源服务器安全配置类

---
+++ jing-security-app/src/main/java/cn/jing/security/server/JingResourceServerConfig.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package cn.jing.security.server;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.social.security.SpringSocialConfigurer;
+
+import cn.jing.security.app.authentication.openid.OpenIdAuthenticationSecurityConfig;
+import cn.jing.security.core.authentication.FormAuthenticationConfig;
+import cn.jing.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import cn.jing.security.core.authorize.AuthorizeConfigManager;
+import cn.jing.security.core.validate.code.ValidateCodeSecurityConfig;
+
+/**
+ * function:资源服务器安全配置类 （继承ResourceServerConfigurerAdapter类）
+ * 
+ * @author liangjing
+ *
+ */
+@Configuration
+@EnableResourceServer
+public class JingResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Autowired
+	protected AuthenticationSuccessHandler imoocAuthenticationSuccessHandler;
+
+	@Autowired
+	protected AuthenticationFailureHandler imoocAuthenticationFailureHandler;
+
+	@Autowired
+	private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+	@Autowired
+	private OpenIdAuthenticationSecurityConfig openIdAuthenticationSecurityConfig;
+
+	@Autowired
+	private ValidateCodeSecurityConfig validateCodeSecurityConfig;
+
+	@Autowired
+	private SpringSocialConfigurer imoocSocialSecurityConfig;
+
+	@Autowired
+	private AuthorizeConfigManager authorizeConfigManager;
+
+	@Autowired
+	private FormAuthenticationConfig formAuthenticationConfig;
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+
+		formAuthenticationConfig.configure(http);
+
+		http.apply(validateCodeSecurityConfig).and().apply(smsCodeAuthenticationSecurityConfig).and()
+				.apply(imoocSocialSecurityConfig).and().apply(openIdAuthenticationSecurityConfig).and().csrf()
+				.disable();
+
+		authorizeConfigManager.config(http.authorizeRequests());
+	}
+
+}
