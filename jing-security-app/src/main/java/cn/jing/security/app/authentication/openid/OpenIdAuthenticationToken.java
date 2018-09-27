5848225a2938a43fdc0ed814aee53bf63e2a3ac7
梁竞 <1184106223@qq.com>
2018-09-27 09:57:14 +0800

    封装app的社交登录的认证信息

---
+++ jing-security-app/src/main/java/cn/jing/security/app/authentication/openid/OpenIdAuthenticationToken.java
@@ -0,0 +1,85 @@
+package cn.jing.security.app.authentication.openid;
+
+import java.util.Collection;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.SpringSecurityCoreVersion;
+
+/**
+ * function:封装app的社交登录的认证信息
+ * 
+ * @author liangjing
+ *
+ */
+public class OpenIdAuthenticationToken extends AbstractAuthenticationToken {
+
+	private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
+
+	// ~ Instance fields
+	// ================================================================================================
+
+	private final Object principal; // openId是放在principal这个属性里边的
+	private String providerId; // 服务提供商的id
+
+	// ~ Constructors
+	// ===================================================================================================
+
+	/**
+	 * This constructor can be safely used by any code that wishes to create a
+	 * <code>UsernamePasswordAuthenticationToken</code>, as the
+	 * {@link #isAuthenticated()} will return <code>false</code>.
+	 *
+	 */
+	public OpenIdAuthenticationToken(String openId, String providerId) {
+		super(null);
+		this.principal = openId;
+		this.providerId = providerId;
+		setAuthenticated(false);
+	}
+
+	/**
+	 * This constructor should only be used by <code>AuthenticationManager</code> or
+	 * <code>AuthenticationProvider</code> implementations that are satisfied with
+	 * producing a trusted (i.e. {@link #isAuthenticated()} = <code>true</code>)
+	 * authentication token.
+	 *
+	 * @param principal
+	 * @param credentials
+	 * @param authorities
+	 */
+	public OpenIdAuthenticationToken(Object principal, Collection<? extends GrantedAuthority> authorities) {
+		super(authorities);
+		this.principal = principal;
+		super.setAuthenticated(true); // must use super, as we override
+	}
+
+	// ~ Methods
+	// ========================================================================================================
+
+	public Object getCredentials() {
+		return null;
+	}
+
+	public Object getPrincipal() {
+		return this.principal;
+	}
+
+	public String getProviderId() {
+		return providerId;
+	}
+
+	public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+		if (isAuthenticated) {
+			throw new IllegalArgumentException(
+					"Cannot set this token to trusted - use constructor which takes a GrantedAuthority list instead");
+		}
+
+		super.setAuthenticated(false);
+	}
+
+	@Override
+	public void eraseCredentials() {
+		super.eraseCredentials();
+	}
+}
