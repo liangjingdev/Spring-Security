2d71d4dba93fceffc50734df541f5ec980f978ed
梁竞 <1184106223@qq.com>
2018-10-12 09:18:17 +0800

    基于session的校验码存取器

--- jing-security-browser/src/main/java/cn/jing/security/browser/validate/code/impl/SessionValidateCodeRepository.java
+++ jing-security-browser/src/main/java/cn/jing/security/browser/validate/code/impl/SessionValidateCodeRepository.java
@@ -3,7 +3,7 @@
  */
 package cn.jing.security.browser.validate.code.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.connect.web.HttpSessionSessionStrategy;
 import org.springframework.social.connect.web.SessionStrategy;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -25,8 +25,7 @@ public class SessionValidateCodeRepository implements ValidateCodeRepository {
 	private static final String SESSION_KEY_PREFIX = "SESSION_KEY_FOR_CODE_";
 
 	// 操作session的工具类
-	@Autowired
-	private SessionStrategy sessionStrategy;
+	private SessionStrategy sessionStrategy = new HttpSessionSessionStrategy();
 
 	/**
 	 * function:保存验证码到session中
