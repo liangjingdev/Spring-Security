c1a71db11eaa01c463b01a84026b2ccd76278bf2
梁竞 <1184106223@qq.com>
2018-10-30 08:30:35 +0800

    api接口

---
+++ jing-security-core/src/main/java/cn/jing/security/core/social/qq/api/QQ.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package cn.jing.security.core.social.qq.api;
+
+/**
+ * function:api接口
+ * 
+ * @author liangjing
+ *
+ */
+public interface QQ {
+
+	/**
+	 * function:获取QQ用户信息
+	 * 
+	 * @return
+	 */
+	QQUserInfo getUserInfo();
+
+}
