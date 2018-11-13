a2bfe2724842c9002031185fd32640eac8ae6a98
梁竞 <1184106223@qq.com>
2018-11-13 19:52:41 +0800

    微信登录的相关配置项

---
+++ jing-security-core/src/main/java/cn/jing/security/core/properties/WeixinProperties.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package cn.jing.security.core.properties;
+
+/**
+ * function:微信登录的相关配置项
+ * 
+ * @author liangjing
+ *
+ */
+public class WeixinProperties {
+
+	/**
+	 * 第三方id，用来决定发起第三方登录的url，默认是 weixin。
+	 */
+	private String providerId = "weixin";
+
+	/**
+	 * Application id.
+	 */
+	private String appId;
+
+	/**
+	 * Application secret.
+	 */
+	private String appSecret;
+
+	public String getAppId() {
+		return this.appId;
+	}
+
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+
+	public String getAppSecret() {
+		return this.appSecret;
+	}
+
+	public void setAppSecret(String appSecret) {
+		this.appSecret = appSecret;
+	}
+
+	/**
+	 * @return the providerId
+	 */
+	public String getProviderId() {
+		return providerId;
+	}
+
+	/**
+	 * @param providerId the providerId to set
+	 */
+	public void setProviderId(String providerId) {
+		this.providerId = providerId;
+	}
+
+}
