a10466156056c7272d5c01ffa437f20709b7b864
梁竞 <1184106223@qq.com>
2018-12-02 10:11:40 +0800

    默认的短信验证码发送器

---
+++ jing-security-core/src/main/java/cn/jing/security/core/validate/code/sms/DefaultSmsCodeSender.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package cn.jing.security.core.validate.code.sms;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * function:默认的短信验证码发送器
+ * 
+ * @author liangjing
+ *
+ */
+public class DefaultSmsCodeSender implements SmsCodeSender {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Override
+	public void send(String mobile, String code) {
+		logger.warn("请配置真实的短信验证码发送器(SmsCodeSender)");
+		logger.info("向手机" + mobile + "发送短信验证码" + code);
+	}
+
+}
