a85d6d507cf73b1dc1724856fbb2519b459cd5be
梁竞 <1184106223@qq.com>
2018-12-09 20:39:31 +0800

    认证成功后的响应方式

---
+++ jing-security-core/src/main/java/cn/jing/security/core/properties/LoginResponseType.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package cn.jing.security.core.properties;
+
+/**
+ * function:认证(登录)成功后的响应方式
+ * 
+ * @author liangjing
+ *
+ */
+public enum LoginResponseType {
+
+	/**
+	 * 跳转
+	 */
+	REDIRECT,
+	/**
+	 * 返回json
+	 */
+	JSON
+
+}
