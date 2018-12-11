c82684a6603c3e19ca5e14f3380543bbe7bb2ace
梁竞 <1184106223@qq.com>
2018-12-12 00:04:44 +0800

    验证码的相关配置项

--- jing-security-core/src/main/java/cn/jing/security/core/properties/ValidateCodeProperties.java
+++ jing-security-core/src/main/java/cn/jing/security/core/properties/ValidateCodeProperties.java
@@ -4,12 +4,21 @@
 package cn.jing.security.core.properties;
 
 /**
+ * function:验证码的相关配置项
+ * 
  * @author liangjing
  *
  */
 public class ValidateCodeProperties {
 
+	/**
+	 * 图形验证码配置
+	 */
 	private ImageCodeProperties image = new ImageCodeProperties();
+	/**
+	 * 短信验证码配置
+	 */
+	private SmsCodeProperties sms = new SmsCodeProperties();
 
 	public ImageCodeProperties getImage() {
 		return image;
@@ -17,6 +26,14 @@ public class ValidateCodeProperties {
 
 	public void setImage(ImageCodeProperties image) {
 		this.image = image;
+	}
+
+	public SmsCodeProperties getSms() {
+		return sms;
+	}
+
+	public void setSms(SmsCodeProperties sms) {
+		this.sms = sms;
 	}
 
 }
