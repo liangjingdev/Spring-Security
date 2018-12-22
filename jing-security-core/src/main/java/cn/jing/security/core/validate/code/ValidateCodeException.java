2c457a74a82063853ee2555b65f7ac46945e586a
梁竞 <1184106223@qq.com>
2018-12-22 14:18:08 +0800

    验证码异常

--- jing-security-core/src/main/java/cn/jing/security/core/validate/code/ValidateCodeException.java
+++ jing-security-core/src/main/java/cn/jing/security/core/validate/code/ValidateCodeException.java
@@ -6,15 +6,14 @@ package cn.jing.security.core.validate.code;
 import org.springframework.security.core.AuthenticationException;
 
 /**
+ * function:验证码处理异常
+ * 
  * @author liangjing
  *
  */
 public class ValidateCodeException extends AuthenticationException {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -6296526574183458021L;
+	private static final long serialVersionUID = -7285211528095468156L;
 
 	public ValidateCodeException(String msg) {
 		super(msg);
