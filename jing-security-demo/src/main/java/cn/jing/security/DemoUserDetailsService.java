1fc762462c7c1980ea6f24cfa7249234ec3af710
梁竞 <1184106223@qq.com>
2019-01-16 22:03:08 +0800

    UserDetailsService接口以及SocialUserDetailsService接口实现类

---
+++ jing-security-demo/src/main/java/cn/jing/security/DemoUserDetailsService.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package cn.jing.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.social.security.SocialUser;
+import org.springframework.social.security.SocialUserDetails;
+import org.springframework.social.security.SocialUserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * function:UserDetailsService接口以及SocialUserDetailsService接口实现类
+ * 
+ * @author liangjing
+ *
+ */
+@Component
+@Transactional
+public class DemoUserDetailsService implements UserDetailsService, SocialUserDetailsService {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//		logger.info("表单登录用户名:" + username);
+//		Admin admin = adminRepository.findByUsername(username);
+//		admin.getUrls();
+//		return admin;
+		return buildUser(username);
+	}
+
+	@Override
+	public SocialUserDetails loadUserByUserId(String userId) throws UsernameNotFoundException {
+		logger.info("社交登录用户Id:" + userId);
+		return buildUser(userId);
+	}
+
+	private SocialUserDetails buildUser(String userId) {
+		// 根据用户名查找用户信息
+		// 根据查找到的用户信息判断用户是否被冻结
+		String password = passwordEncoder.encode("123456");
+		logger.info("数据库密码是:" + password);
+
+		return new SocialUser(userId, password, true, true, true, true,
+				AuthorityUtils.commaSeparatedStringToAuthorityList("xxx"));
+	}
+
+}
